c992499bd4dea6edf06e2783e88e75f35e6ce414
Alex228-usa <alex.johnsonwht@gmail.com>
2024-02-12 15:01:42 +0600

    improve code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <div>
+      <select v-model="selectedCountry" @change="onCountryChanged">
+        <!-- Добавляем v-bind:key к каждой опции -->
+        <option v-for="country in countries" :key="country.code" :value="country.code">{{ country.name }}</option>
+      </select>
+      <phone-number-input
+        v-model="phoneNumber"
+        :country="selectedCountry"
+        @input="validatePhoneNumber"
+      ></phone-number-input>
+      <span v-if="!validPhoneNumber" style="color: red;">Неверный номер телефона</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PhoneNumberInput from 'vue-phone-number-input';
+import 'vue-phone-number-input/dist/vue-phone-number-input.css';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PhoneNumberInput,
+  },
+  data() {
+    return {
+      phoneNumber: '',
+      validPhoneNumber: true,
+      selectedCountry: 'US', // Задайте страну по умолчанию
+      countries: [ // Список стран
+        { code: 'US', name: 'United States' },
+        { code: 'GB', name: 'United Kingdom' },
+        // Другие страны...
+      ]
+    };
+  },
+  methods: {
+    onCountryChanged() {
+      // Действия при изменении страны
+    },
+    validatePhoneNumber() {
+      // Валидация номера телефона
+      this.validPhoneNumber = this.$refs.phoneNumberInput.phoneNumberValid();
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*  стили */
 </style>
---
+++ src/components/PhoneNumberInput.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <phone-number-input
+        v-model="phoneNumber"
+        @country-changed="onCountryChanged"
+        @input="validatePhoneNumber"
+      ></phone-number-input>
+      <span v-if="!validPhoneNumber" style="color: red;">Неверный номер телефона</span>
+    </div>
+  </template>
+  
+  <script>
+  import PhoneNumberInput from 'vue-phone-number-input';
+  import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+  
+  
+  export default {
+    name: 'PhoneNumberInput',
+    components: {
+      PhoneNumberInput,
+    },
+    data() {
+      return {
+        phoneNumber: '',
+        validPhoneNumber: true,
+      };
+    },
+    methods: {
+      onCountryChanged(countryCode) {
+        // Действия при изменении страны
+        console.log('Выбрана страна:', countryCode);
+      },
+      validatePhoneNumber() {
+        // Валидация номера телефона
+        this.validPhoneNumber = this.$refs.phoneNumberInput.phoneNumberValid();
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /*  добавить стилизацию компонента здесь */
+  </style>
+  
